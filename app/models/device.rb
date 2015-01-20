62ade5d47b7635a5a7bd1cd534c92320af1033bb
Jeremy Senn <senn.jeremy@gmail.com>
2015-01-20 06:02:53 -0800

    devices remaining for bins and total remaining

--- app/models/device.rb
+++ app/models/device.rb
@@ -34,7 +34,8 @@ class Device < ActiveRecord::Base
       bill_counts.take.host_start_count - bill_counts.take.host_cycle_count
     else
       return nil
-    end  end
+    end  
+  end
   
   def bin_5
     bill_counts = BillCount.where(dev_id: id, cassette_nbr: 5, cassette_id: 5)
@@ -42,7 +43,8 @@ class Device < ActiveRecord::Base
       bill_counts.take.host_start_count - bill_counts.take.host_cycle_count
     else
       return nil
-    end  end
+    end  
+  end
   
   def bin_6
     bill_counts = BillCount.where(dev_id: id, cassette_nbr: 6, cassette_id: 6)
@@ -50,7 +52,8 @@ class Device < ActiveRecord::Base
       bill_counts.take.host_start_count - bill_counts.take.host_cycle_count
     else
       return nil
-    end  end
+    end  
+  end
   
   def bin_7
     bill_counts = BillCount.where(dev_id: id, cassette_nbr: 7, cassette_id: 7)
@@ -58,7 +61,8 @@ class Device < ActiveRecord::Base
       bill_counts.take.host_start_count - bill_counts.take.host_cycle_count
     else
       return nil
-    end  end
+    end  
+  end
   
   def bin_8
     bill_counts = BillCount.where(dev_id: id, cassette_nbr: 8, cassette_id: 8)
@@ -66,5 +70,82 @@ class Device < ActiveRecord::Base
       bill_counts.take.host_start_count - bill_counts.take.host_cycle_count
     else
       return nil
-    end  end
+    end  
+  end
+  
+  def bin_1_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 1, cassette_id: 1)
+    if denominations.blank? or bin_1.blank?
+      return 0
+    else
+      return bin_1 * denominations.take.denomination
+    end
+  end
+  
+  def bin_2_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 2, cassette_id: 2)
+    if denominations.blank? or bin_2.blank?
+      return 0
+    else
+      return bin_2 * denominations.take.denomination
+    end
+  end
+  
+  def bin_3_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 3, cassette_id: 3)
+    if denominations.blank? or bin_3.blank?
+      return 0
+    else
+      return bin_3 * denominations.take.denomination
+    end
+  end
+  
+  def bin_4_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 4, cassette_id: 4)
+    if denominations.blank? or bin_4.blank?
+      return 0
+    else
+      return bin_4 * denominations.take.denomination
+    end
+  end
+  
+  def bin_5_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 5, cassette_id: 5)
+    if denominations.blank? or bin_5.blank?
+      return 0
+    else
+      return bin_5 * denominations.take.denomination
+    end
+  end
+  
+  def bin_6_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 6, cassette_id: 6)
+    if denominations.blank? or bin_6.blank?
+      return 0
+    else
+      return bin_6 * denominations.take.denomination
+    end
+  end
+  
+  def bin_7_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 7, cassette_id: 7)
+    if denominations.blank? or bin_7.blank?
+      return 0
+    else
+      return bin_7 * denominations.take.denomination
+    end
+  end
+  
+  def bin_8_remaining
+    denominations = Denom.where(dev_id: id, cassette_nbr: 8, cassette_id: 8)
+    if denominations.blank? or bin_8.blank?
+      return 0
+    else
+      return bin_8 * denominations.take.denomination
+    end
+  end
+  
+  def remaining
+    return bin_1_remaining + bin_2_remaining + bin_3_remaining + bin_4_remaining + bin_5_remaining + bin_6_remaining + bin_7_remaining + bin_8_remaining
+  end
 end
