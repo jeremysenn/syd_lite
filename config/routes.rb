e730da65300fa725667e7c0060b4b182236b8a3b
Jeremy Senn <senn.jeremy@gmail.com>
2015-01-23 14:35:45 -0800

    Remove all routes except for devices and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :transactions
   resources :devices
-  resources :cards
-  resources :dev_statuses
+#  resources :transactions
+#  resources :cards
+#  resources :dev_statuses
+
   resources :sessions
 
   # You can have the root of your site routed with "root"
