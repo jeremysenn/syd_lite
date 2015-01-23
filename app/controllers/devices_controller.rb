ae793a8d1d9b9caaf3ae742016552b1b18657ba5
Jeremy Senn <senn.jeremy@gmail.com>
2015-01-23 14:42:26 -0800

    make cassette statuses a smaller panel

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -17,7 +17,7 @@ class DevicesController < ApplicationController
   def show
     @device = Device.find(params[:id])
     @transactions = Kaminari.paginate_array(@device.transactions.order(transactions_sort_column + ' ' + transactions_sort_direction)).page(params[:transactions_page]).per(15)
-    @dev_statuses = Kaminari.paginate_array(@device.dev_statuses.order("date_time DESC")).page(params[:dev_statuses_page]).per(15)
+    @dev_statuses = Kaminari.paginate_array(@device.dev_statuses.order("date_time DESC")).page(params[:dev_statuses_page]).per(10)
     @cards = Kaminari.paginate_array(@device.cards.order(cards_sort_column + ' ' + cards_sort_direction)).page(params[:cards_page]).per(15)
     @bill_hists = Kaminari.paginate_array(@device.bill_hists.order("cut_dt DESC")).page(params[:bill_hists_page]).per(15)
     
