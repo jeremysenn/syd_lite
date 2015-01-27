e730d76d4ac26ad7b053c8af1d0125e8fc3863f5
Jeremy Senn <senn.jeremy@gmail.com>
2015-01-27 06:19:31 -0800

    added device state descriptions array

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
   
+  def device_state_descriptions
+    ["Ready", "Disconnected", "Waiting for Up response", "Waiting for Down response", "Waiting for Status", "Down", "Waiting for Transaction reply", 
+      "Waiting for Down response (to begin load)", "Loading...", "Waiting for Config", "Supervisor mode", "Wait Reset", "Wait Read Cassettes", 
+      "Wait Present", "Wait Totals", "Wait Reset Totals", "Wait Present", "Wait Test Dispense", "Wait Test Reject", "Wait Totals pre-Dispense", 
+      "Wait Totals after Timeout", "Wait for Reset on bad dispense", "No Server", "Unknown", "Wait for ID", "Wait for Read Cassettes before redispense", 
+      "Waiting for notes to be rejected", "Waiting for notes to be rejected", "Waiting for Lifts Up", "Waiting for new key", "Waiting for user response", 
+      "Waiting for Bill Pay screen", "Waiting for bill pay amount", "Waiting for Bill Pay Confirmation", ""]
+  end
+  
   def transactions_sortable(column, title = nil)
     title ||= column.titleize
     css_class = (column == transactions_sort_column) ? "current #{transactions_sort_direction}" : nil
@@ -28,11 +37,14 @@ module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
-              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+            concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message 
             end)
-    end
-    nil
-  end
+        end
+        nil
+      end
   
-end
+    end
+
+    
+    
