7ecc17ea3417e2ad57ec2ca9da414912c5b6ca1a
Jeremy Senn <senn.jeremy@gmail.com>
2015-01-28 08:58:43 -0800

    show more bill_hists in cuts pagination

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -32,7 +32,7 @@ class DevicesController < ApplicationController
     @transactions = Kaminari.paginate_array(@device.transactions.order(transactions_sort_column + ' ' + transactions_sort_direction)).page(params[:transactions_page]).per(15)
     @dev_statuses = Kaminari.paginate_array(@device.dev_statuses.order("date_time DESC")).page(params[:dev_statuses_page]).per(10)
     @cards = Kaminari.paginate_array(@device.cards.order(cards_sort_column + ' ' + cards_sort_direction)).page(params[:cards_page]).per(10)
-    @bill_hists = Kaminari.paginate_array(@device.bill_hists.order("cut_dt DESC")).page(params[:bill_hists_page]).per(10)
+    @bill_hists = Kaminari.paginate_array(@device.bill_hists.order("cut_dt DESC")).page(params[:bill_hists_page]).per(40)
     
     # Determine if we can eliminate any empty columns
     @error_code_column_count = @transactions.select { |result| result.error_code != "" }.select{ |result| result.error_code != nil }.count
